8bc6a2680fcb65ce274b7c60199ba41f408dd3a3
Charlotte Quinn <cquinn03@seattlecentral.edu>
2014-04-26 13:44:23 -0700

    Update index.php

--- assignment-02/index.php
+++ assignment-02/index.php
@@ -1,10 +1,16 @@
 <!doctype html>
-<html>
-<head><title>Mad Libs</title>
-</head>
-<body>
-WELCOME TO YOUR MUSICAL MADLIBS!
-<form method="post">
+	<html>
+<?php
+$method = $_SERVER["REQUEST_METHOD"];
+echo $method;
+//this will tellyou if you are getting or posting?
+if ($method == "GET")
+{
+
+                                                                          //we open the if
+?>
+	<form method="post">
+
 Favorite activity (with out the '-ing' ending)<input placeholder="verb" name="verb"><br>
 Type of Pet you had as a child<input placeholder="noun" name="noun"><br>
 Name of your childhood pet<input placeholder="pet" name="pet"><br>
@@ -20,29 +26,43 @@ Any Number<input placeholder="number2" name="number2"><br>
 	<input type="radio" value="music" name="deathmetal">
 	<label for="deathmetal">DEATH METAL</label>
 <br>
+
 	<button>POST</button>
 	</form>
+	
+<?php 
+}
+else{
+											//we close the if and add an else (close else later);
+?>
 
+	<pre>
+<?php print_r($_REQUEST);?>
+	</pre>
 
+	<br>
+	Your name is <?= $_REQUEST["first_name"]?>
 <?php
-if (isset($_REQUEST["rockroll"])){
+$number2 = $_REQUEST["number2"];
+$afteronehundred = $age - 100;
+if ($number2 >=100){
+?>
+					You are over 100 by <?=$afteronehundred?> years.
+<?php
+} else{
+?>
+				you are over 100, what are you doing on line?
+<?php
+	}
+?>
+
+<a href="index2.php">GET</a>
+<?php
+}
+
+if (isset($_REQUEST["rock"])){
 echo "You probably love Elvis Presley!";
 echo "<br><img src='images/Elvis.jpg'>";
-}
-
-else if (isset($_REQUEST["jazz"])){
-echo "You probably love Miles Davis!";
-echo "<br><img src='images/miles.jpg'>";
-}
-
-else if (isset($_REQUEST["deathmetal"])){
-echo "You probably like White Chapel! ";
-echo "<br><img src='images/white.jpg'>";
-
-}
-
-?>
-<?php
 $verb = $_REQUEST['verb'];
 $noun = $_REQUEST['noun'];
 $pet = $_REQUEST['pet'];
@@ -50,26 +70,19 @@ $maiden = $_REQUEST['maiden'];
 $adjective = $_REQUEST['adjective'];
 $number = $_REQUEST['number'];
 $number2 = $_REQUEST['number2'];
-/*
-
-if(empty($verb) || empty($noun) || empty($pet) || empty($maiden || empty($adjective) || empty($number) || empty($number2))
-echo "Sorry, fill out all the fields."
 }
-else
-{
-
-*/
-echo "If you were their lead singer, you would be called " .  $verb . "ing"  . $adjective  . $noun . "!";
-echo "Your true age is " . $number + " although you pretend to be " . $number2;
-echo "If you were a female impersonator your name would be " . $pet . $maiden;
-
-
-/*
-echo "If you were their lead singer, you would be called " .  $verb . "ing"  . $adjective  . $noun . "!";
-echo "Your true age is " . $number + " although you pretend to be " . $number2;
-echo "If you were a female impersonator your name would be " . $pet . $maiden;
-*/
+else if (isset($_REQUEST["jazz"])){
+echo "You probably love Miles Davis!";
+echo "<br><img src='images/miles.jpg'>";
+}
+else if (isset($_REQUEST["deathmetal"])){
+echo "You probably like White Chapel! ";
+echo "<br><img src='images/white.jpg'>";
+echo "If you were their lead singer, you would be called " +  $verb + "ing"  + $adjective  + $noun + "!";
+echo "Your true age is " + $number + " although you pretend to be " + $number2;
+echo "If you were a female impersonator your name would be " + $pet + $maiden;
+}
 ?>
+	<!-- now you'll never see the form on the page at the same time as you see that get?-->
 
-</body>
-</html>
+	</html>
